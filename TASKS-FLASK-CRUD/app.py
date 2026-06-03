c61a2b9b4ea17457abf1ecb7f05d99ae7f7f2c4a
kenzinho1 <kenzinhohiga@gmail.com>
2026-06-03 15:39:08 -0400

    Crud Read

--- TASKS-FLASK-CRUD/app.py
+++ TASKS-FLASK-CRUD/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
+from models.task import Task
 app = Flask(__name__)
 
 # CRUD
@@ -7,7 +7,27 @@ app = Flask(__name__)
 
 
 tasks = []
-@app.route("/")
+task_id_control = 1 # criamos essa variavel fora, pois se criassemos dentro do metodo acabaria resetando e tendo varias tasks com id 1
+
+@app.route("/tasks", methods=["POST"])
+def create_task():
+    global task_id_control # usado para o programa conseguir usar uma variavel de fora do metodo.
+    data = request.get_json()
+    new_task = Task(id=task_id_control, title=data['title'], description=data.get('description', ""))
+    task_id_control += 1 # essa adição acaba sendo necessário usar o global para conseguirmos usar a task_id_control
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message": "Nova tarefa criada com sucesso"})
+
+@app.route("/tasks", methods=["GET"])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks ]
+
+    output = {
+                  "tasks": task_list,
+                  "total_tasks": 0
+            }
+    return jsonify(output)
 
 if __name__ == "__main__":
     app.run(debug=True)
